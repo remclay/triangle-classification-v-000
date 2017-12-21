d718ec30600e1948505456df47e9e0b713a4af6b
Remmy Clay <remmy.lauren@gmail.com>
2017-12-21 17:58:59 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind
-    if (@a <= 0 || @b <= 0 || @c <= 0) ||
+    if (a <= 0 || b <= 0 || c <= 0) ||
       ((@a + @b <= @c)||(@b + @c <= @a)||(@a + @c <= @b))
       begin
         raise TriangleError
