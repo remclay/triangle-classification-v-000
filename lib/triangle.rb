f0129fec67a412cd630578c6f9d2ee02365a3b97
Remmy Clay <remmy.lauren@gmail.com>
2017-12-21 17:31:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,18 +7,23 @@ class Triangle
   end
 
   def kind
-    if (@side_1 == @side_2 && @side_2 == @side_3)
-      :equilateral
-    elsif (@side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3)
-      :isosceles
-    elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
-      :scalene
-    end
     if (@side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0) ||
       ((@side_1 + @side_2 < @side_3)||(@side_2 + @side_3 < @side_1)||(@side_1 + @side_3 < @side_2))
-    
+      begin
+        raise TriangleError
+      rescue
+      end
+    else
+      if (@side_1 == @side_2 && @side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3)
+        :scalene
+      end
+    end
   end
-  
+
 end #end Triangle class
 
 class TriangleError < StandardError
