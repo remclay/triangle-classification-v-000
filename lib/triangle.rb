c4dcf4bf1c9f8271889d70877bf53abc3640490f
Remmy Clay <remmy.lauren@gmail.com>
2017-12-21 17:31:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
       ((@side_1 + @side_2 < @side_3)||(@side_2 + @side_3 < @side_1)||(@side_1 + @side_3 < @side_2))
       begin
         raise TriangleError
-      rescue
+  #    rescue
       end
     else
       if (@side_1 == @side_2 && @side_2 == @side_3)
